d6615c56d561d14605b43f62655c4d4a7ce19378
PRIYA <priya9899nk@gmail.com>
2026-04-27 20:27:08 +0530

    Fix formatting of dotenv import in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv")
 
 const caseRoutes = require("./routes/CaseRoutes");
 const volunteerRoutes = require("./routes/VolunteerRoutes");
